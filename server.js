fbb254ef630621fac46f973e3a36945263bade8d
Abhishek Maurya <technicalyarana@gmail.com>
2023-11-05 01:48:25 -0700

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
-const app = require('./app')
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
 
-const port = process.env.PORT || 3000
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// Serve your HTML, CSS, and JavaScript files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// WebSocket server code
+wss.on('connection', (ws) => {
+  console.log('WebSocket client connected');
+
+  // WebSocket event handlers
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    
+    // Send a response back to the client
+    ws.send(`Server received: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('WebSocket client disconnected');
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
